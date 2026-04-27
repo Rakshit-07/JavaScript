1a8beaaa9ac99eabc59622f3e43dadb6a2008666
Rakshit <rnigam796@gmail.com>
2026-04-27 07:23:18 +0530

    Added new code

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,93 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+//if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+
+//true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+
+const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Nigam";
+console.log(lastName);
+
+*/
+
+const now = 2037;
+const ageJonas = now - 1996;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Rakshit";
+const lastName = "Nigam";
+console.log(firstName + " " + lastName);
+
+//Assignment operator
+let x = 10 + 5;
+x += 10;
+x += 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison operator
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
